ca43086d785f19eb64156f9a5fccb55e59cdc018
andrey-zaytcev <andrey-zaytcev@narod.ru>
2016-10-09 21:35:42 +0300

    gnu global support added

--- init.el
+++ init.el
@@ -1,2 +1,13 @@
 (setq-default c-basic-offset 4)
 (global-linum-mode t)
+
+(require 'package)
+(add-to-list 'package-archives
+       '("melpa" . "http://melpa.milkbox.net/packages/") t)
+
+(add-hook 'c-mode-common-hook
+    (lambda ()
+      (when (derived-mode-p 'c-mode 'c++-mode 'java-mode 'asm-mode)
+  (ggtags-mode 1))))
+
+(add-hook 'dired-mode-hook 'ggtags-mode)
