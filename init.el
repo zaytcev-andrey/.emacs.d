710a788d758af32804183b1fd449b4d383afa03b
andrey-zaytcev <andrey-zaytcev@narod.ru>
2016-11-08 21:19:19 +0300

    added require packages: volatile-highlights, undo-tree, sr-speedbar, helm

--- init.el
+++ init.el
@@ -7,11 +7,20 @@
 ;; add your modules path
 (add-to-list 'load-path "~/.emacs.d/custom/")
 
+;; highlights changes in buffer
 (require 'volatile-highlights)
 (volatile-highlights-mode t)
 
 (require 'undo-tree)
 (global-undo-tree-mode)
+(defalias 'redo 'undo-tree-redo)
+(global-set-key (kbd "C-z") 'undo)
+(global-set-key (kbd "C-S-z") 'redo)
+(defadvice undo-tree-visualize (around undo-tree-split-side-by-side activate)
+	"Split undo-tree side-by-side."
+	(let ((split-height-threshold nil)
+		  (split-width-threshold 0))
+	  ad-do-it))
 
 (require 'sr-speedbar)
 (setq sr-speedbar-right-side nil)
@@ -48,6 +57,7 @@
 (setq-default tab-width 4)
 (setq-default c-basic-offset 4)
 (global-linum-mode t)
+(windmove-default-keybindings)
 
 ;; GROUP: Editing -> Killing
 (setq
@@ -78,3 +88,68 @@
 (add-hook 'dired-mode-hook 'ggtags-mode)
 
 (setq-local imenu-create-index-function #'ggtags-build-imenu-index)
+
+;; compile by F7 key
+(global-set-key (kbd "<f7>") (lambda ()
+                               (interactive)
+                               (setq-local compilation-read-command nil)
+                               (call-interactively 'compile)))
+
+(setq
+ ;; use gdb-many-windows by default
+ gdb-many-windows t
+
+ ;; Non-nil means display source file containing the main routine at startup
+ gdb-show-main t
+ )
+
+;; Helm
+(require 'helm)
+(require 'helm-config)
+
+;; The default "C-x c" is quite close to "C-x C-c", which quits Emacs.
+;; Changed to "C-c h". Note: We must set "C-c h" globally, because we
+;; cannot change `helm-command-prefix-key' once `helm-config' is loaded.
+(global-set-key (kbd "C-c h") 'helm-command-prefix)
+(global-set-key (kbd "C-c h o") 'helm-occur)
+(global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "C-x b") 'helm-mini)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-unset-key (kbd "C-x c"))
+
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
+(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB work in terminal
+(define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
+
+(when (executable-find "curl")
+  (setq helm-google-suggest-use-curl-p t))
+
+(setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
+      helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
+      helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
+      helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
+      helm-ff-file-name-history-use-recentf t
+      helm-echo-input-in-header-line t)
+
+(defun spacemacs//helm-hide-minibuffer-maybe ()
+  "Hide minibuffer in Helm session if we use the header line as input field."
+  (when (with-helm-buffer helm-echo-input-in-header-line)
+    (let ((ov (make-overlay (point-min) (point-max) nil nil t)))
+      (overlay-put ov 'window (selected-window))
+      (overlay-put ov 'face
+                   (let ((bg-color (face-background 'default nil)))
+                     `(:background ,bg-color :foreground ,bg-color)))
+      (setq-local cursor-type nil))))
+
+
+(add-hook 'helm-minibuffer-set-up-hook
+          'spacemacs//helm-hide-minibuffer-maybe)
+
+(setq helm-autoresize-max-height 0)
+(setq helm-autoresize-min-height 20)
+(helm-autoresize-mode 1)
+
+(setq helm-semantic-fuzzy-match t
+      helm-imenu-fuzzy-match    t)
+
+(helm-mode 1)
